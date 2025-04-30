89f5606a37d5894775db5b928f46951f8b9ccca4
Rushikeswar <rushikesward@gmail.com>
2025-04-30 23:25:02 +0530

    redis connectec

---
+++ redisClient.js
@@ -0,0 +1,22 @@
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
+const client = createClient({
+    username: process.env.REDIS_USER,
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    }
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+
+export default redisClient;
+
+// await client.set('foo', 'bar');
+// const result = await client.get('foo');
+// console.log(result)  // >>> bar
+
